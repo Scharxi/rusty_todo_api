459a1b43fb5c177db51f03b240250ef828d0c12d
BufferOverflow <scharxidev@outlook.de>
2023-01-15 15:37:51 +0100

    fetch all records route

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
 mod model;
 mod response;
+mod handler;
 
 use actix_web::{App, get, HttpResponse, HttpServer, Responder};
 use actix_web::middleware::Logger;
-use crate::response::GenericResponse;
 
-
-#[get("api/healthcheck")]
-async fn health_checker_handler() -> impl Responder {
-    const MESSAGE: &str = "Build simple rest CRUD api with actix";
-
-    let response_json = &GenericResponse {
-        status: "success".to_string(),
-        message: MESSAGE.to_string(),
-    };
-
-    HttpResponse::Ok().json(response_json)
-}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -29,7 +17,7 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
-            .service(health_checker_handler)
+            .service(handler::health_checker_handler)
             .wrap(Logger::default())
     })
         .bind(("127.0.0.1", 8000))?
